bdb2e12e8eb5ad59cc016c629a80afd5c84baeb3
EdgarsLv <edgars0707@gmail.com>
2025-06-29 23:55:42 +0300

    added tech icons

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
 
+type Icon = {
+  icon: string;
+  name: string;
+};
+
 @Component({
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  iconSize = 36;
+
+  icons: Icon[] = [
+    { icon: 'angular', name: 'Angular' },
+    { icon: 'react', name: 'React' },
+    { icon: 'reactnative', name: 'React Native' },
+    { icon: 'javascript', name: 'JavaScript' },
+    { icon: 'typescript', name: 'TypeScript' },
+    { icon: 'net', name: '.NET' },
+    { icon: 'css', name: 'CSS3' },
+    { icon: 'git', name: 'Git' },
+    { icon: 'firebase', name: 'Firebase' },
+    { icon: 'supabase', name: 'Supabase' },
+    { icon: 'redux', name: 'Redux' },
+    { icon: 'zustand', name: 'Zustand' },
+    { icon: 'tailwind', name: 'Tailwind CSS' },
+    { icon: 'mysql', name: 'MySQL' },
+    { icon: 'postgresql', name: 'PostgreSQL' },
+    { icon: 'inkscape', name: 'Inkscape' },
+  ];
+}
