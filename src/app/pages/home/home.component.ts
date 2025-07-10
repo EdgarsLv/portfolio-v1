86289de66247bd580ffc49471c99b720f4cb5d0b
EdgarsLv <edgars0707@gmail.com>
2025-07-10 21:38:01 +0300

    adjust styling for footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { gsap } from 'gsap';
 import SplitText from 'gsap/SplitText';
+import { ScrollDownComponent } from '../../components/scroll-down/scroll-down.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ScrollDownComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements AfterViewInit {
+  public socialIconSize = 24;
   ngAfterViewInit() {}
 }
