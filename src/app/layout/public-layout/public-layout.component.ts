eed67e605aa78e339adecde96143df755479622b
EdgarsLv <edgars0707@gmail.com>
2025-07-14 00:36:03 +0300

    adjust section middle point

--- src/app/layout/public-layout/public-layout.component.ts
+++ src/app/layout/public-layout/public-layout.component.ts
@@ -3,11 +3,17 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { LoaderService } from '../../services/loader.service';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-// import { LoaderScreenComponent } from '../../components/loader-screen/loader-screen.component';
+import { LoaderScreenComponent } from '../../components/loader-screen/loader-screen.component';
 
 @Component({
   selector: 'app-public-layout',
-  imports: [RouterOutlet, RouterModule, FooterComponent, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    FooterComponent,
+    HeaderComponent,
+    LoaderScreenComponent,
+  ],
   templateUrl: './public-layout.component.html',
   styleUrl: './public-layout.component.css',
 })
